5826ff8783ae990986aeff41d39e4186607f5c16
Quertyy <qwertyense@protonmail.com>
2023-02-03 14:06:36 +0100

    refactor: move args parser to helper func

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+pub struct Config {
+    pub contract_address: String,
+    pub contract_abi: String,
+    pub price: u128,
+    pub amount: u128,
+    pub private_key: String,
+    pub timestamp: u128,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let contract_address = args[1].clone();
+        let contract_abi = args[2].clone();
+        let price = args[3].parse().unwrap();
+        let amount = args[4].parse().unwrap();
+        let private_key = args[5].clone();
+        let timestamp = args[6].parse().unwrap();
+
+        Ok(Config {
+            contract_address,
+            contract_abi,
+            price,
+            amount,
+            private_key,
+            timestamp,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 use web3_unit_converter::Unit;
 use web3::types::Address;
 use std::env;
-use web3::api::Txpool;
 
 
 use clap::{arg, command, value_parser, ArgAction, Command, Arg};
 
 #[tokio::main]
 async fn main() -> web3::Result<()> {
-    let matches = Command::new("Snipe this mint!")
+    
+    helper();
+    let args: Vec<String> = env::args().collect();
+    dotenv::dotenv().ok();
+
+    let url = &env::var("ALCHEMY_URL").unwrap();
+    let address = &env::var("ADDRESS").unwrap();
+
+    get_user_balance(address, url).await?;
+    Ok(())
+}
+
+fn helper() {
+    Command::new("Snipe this mint!")
         .about("A simple CLI tool to mint NFTs on Ethereum")
         .version("0.1.0")
         .author("Querty for The Diggers DAO")
@@ -49,19 +61,8 @@ async fn main() -> web3::Result<()> {
                 .required(true),
         )
         .get_matches();
-    
-    let args: Vec<String> = env::args().collect();
-    dotenv::dotenv().ok();
-
-    let url = &env::var("ALCHEMY_URL").unwrap();
-    let address = &env::var("ADDRESS").unwrap();
-
-    get_user_balance(address, url).await?;
-    Ok(())
 }
 
-
-// récupérer la balance d'un utilisateur
 async fn get_user_balance(user: &str, url: &str) -> web3::Result<()> {
     let account: Address = user.parse().unwrap();
     
@@ -76,18 +77,3 @@ async fn get_user_balance(user: &str, url: &str) -> web3::Result<()> {
 
     Ok(())
 }
-
-// TODO
-// check les tx dans la mempool
-//async fn get_mempool() -> web3::Result<()> {
-//    let url = &env::var("ALCHEMY_URL").unwrap();
-//
-//    let _ = env_logger::try_init();
-//    let transport = web3::transports::Http::new(url)?;
-//    let web3 = web3::Web3::new(transport);
-//
-//    let mempool = web3.eth().pending_transactions().await?;
-//    println!("Mempool: {:?}", mempool);
-//
-//    Ok(())
-//}
