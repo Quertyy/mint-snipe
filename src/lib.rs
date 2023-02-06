d4159845fdf7c0a4db50aca21517cba366e7a49f
Quertyy <qwertyense@protonmail.com>
2023-02-06 21:46:21 +0100

    feat: add timestamp, balance checks + config parser done

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,145 @@
+use clap::Parser;
+
+use std::time::{SystemTime};
+use std::fs::File;
+use std::io::Read;
+use std::env;
+use std::error::Error;
+
+use web3::{Web3, contract};
+use web3::futures::Future;
+use web3::contract::{Contract, Options};
+use web3::transports::Http;
+use web3::types::{Address, U256, H160};
+use web3_unit_converter::Unit;
+use serde_json::{Result, Value};
+
+#[derive(Parser)]
+#[command(name = "Snipe this Mint!")]
+#[command(author = "Querty for The Diggers DAO")]
+#[command(version = "0.1.0")]
+#[command(about = "Does awesome things", long_about = None)]
 pub struct Config {
-    pub contract_address: String,
-    pub contract_abi: String,
-    pub price: u128,
-    pub amount: u128,
-    pub timestamp: u128,
+    // The contract mint address
+    #[arg(short, long)]
+    contract_address: String,
+    // The contract mint method
+    #[arg(short, long)]
+    mint_method: String,
+    // The mint price
+    #[arg(short, long)]
+    price: u64,
+    // Number of NFT you want to mint
+    #[arg(short, long)]
+    amount: u64,
+    // The timestamp of the beginning of the mint
+    #[arg(short, long)]
+    timestamp: u64,
 }
 
-impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
+pub struct User {
+    pub url: String,
+    pub address: String,
+}
+
+impl User {
+    pub fn parse() -> User {
+        dotenv::dotenv().ok();
+
+        let url = &env::var("ALCHEMY_URL").unwrap();
+        let address = &env::var("ADDRESS").unwrap();
+
+        User {
+            url: url.to_string(),
+            address: address.to_string(),
         }
-
-        let contract_address = args[1].clone();
-        let contract_abi = args[2].clone();
-        let price = args[3].parse().unwrap();
-        let amount = args[4].parse().unwrap();
-        let timestamp = args[5].parse().unwrap();
-
-        Ok(Config {
-            contract_address,
-            contract_abi,
-            price,
-            amount,
-            timestamp,
-        })
     }
 }
+
+pub fn init_connection() -> web3::Result<Web3<Http>> {
+    let user = User::parse();
+    let url = &user.url;
+    let _ = env_logger::try_init();
+
+    let transport = Http::new(url)?;
+    let web3 = Web3::new(transport);
+
+    Ok(web3)
+}
+
+pub async fn check_on_config(config: &Config, user: &str, web3: &web3::Web3<Http>) -> web3::Result<()> {
+    check_timestamp_requirement(config).unwrap();
+    check_balance_requirement(config, web3, user).await.unwrap();
+
+    Ok(())
+}
+
+fn check_timestamp_requirement(config: &Config) -> Result<()> {
+    let timestamp = get_unix_time();
+    if timestamp > config.timestamp {
+        panic!("The mint has already started!");
+    } else if timestamp < config.timestamp - 300 { // must be run 5 minutes or less before the mint
+        panic!("The mint is too far in the future!");
+    }
+
+    Ok(())
+}
+
+async fn check_balance_requirement(config: &Config, web3: &web3::Web3<Http>, user: &str) -> web3::Result<()> {
+    let account: Address = user.parse().unwrap();
+
+    let wei_balance = web3.eth().balance(account, None).await?;
+    let eth_balance = Unit::Wei(&wei_balance.to_string()).to_eth_str().unwrap();
+    println!("Balance of {:?}: {} ETH", account, eth_balance);
+
+    if wei_balance.as_u64() < config.price * config.amount {
+        panic!("You don't have enough ETH to mint!");
+    }
+
+    Ok(())
+}
+
+pub fn get_unix_time() -> u64 {
+    SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()
+}
+
+pub async fn get_user_balance(web3: &web3::Web3<Http>, user: &str) -> web3::Result<u64> {
+    let account: Address = user.parse().unwrap();
+
+    let wei_balance = web3.eth().balance(account, None).await?;
+    let eth_balance = Unit::Wei(&wei_balance.to_string()).to_eth_str().unwrap();
+    println!("Balance of {:?}: {} ETH", account, eth_balance);
+    Ok(wei_balance.as_u64())
+}
+
+pub fn read_abi() -> Result<Value>{
+    let mut file = File::open("abi/test.json").unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    
+
+    let abi_json: Value = serde_json::from_str(&contents).unwrap();
+    let abi_string = abi_json["result"].as_str().unwrap();
+    let abi: Value = serde_json::from_str(abi_string).unwrap();
+    Ok(abi)
+}
+
+/*
+async fn get_contract_abi(web3: &web3::Web3<Http>, contract_address: H160) -> bool {
+    let code = web3.eth().code(contract_address, None).await.unwrap();
+    if code.is_empty() {
+        return false;
+    }
+    let abi = web3.eth().get_abi(contract_address, None).await.unwrap();
+    if abi.is_none() {
+        return false;
+    }
+    let contract = Contract::from_abi(web3.eth(), contract_address, abi.unwrap(), Options::default()).unwrap();
+    let symbol: H256 = contract.function("symbol").call().await.unwrap();
+    if symbol.is_zero() {
+        return false;
+    }
+    true
+}
+*/
--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,17 @@
-use web3_unit_converter::Unit;
-use web3::transports::Http;
-use web3::types::{Address, U256};
-use std::env;
-use std::time::{SystemTime};
-use std::error::Error;
+use clap::Parser;
 
-// abi read
-use serde_json::{Result, Value};
-use std::fs::File;
-use std::io::Read;
-
-// args parser
-use clap::{arg, command, value_parser, ArgAction, Command, Arg};
+use mint_sniper::*;
 
 #[tokio::main]
 async fn main() -> web3::Result<()> {
-    dotenv::dotenv().ok();
-    helper();
+    let config = Config::parse();
+    let user = User::parse();
 
-    let url = &env::var("ALCHEMY_URL").unwrap();
-    let address = &env::var("ADDRESS").unwrap();
-
-    let _ = env_logger::try_init();
-    let transport = web3::transports::Http::new(url)?;
-    let web3 = web3::Web3::new(transport);
-    
-    
-    let args: Vec<String> = env::args().collect();
-    
-
-    get_user_balance(&web3, address).await?;
-    let timestamp = get_unix_time();
-    println!("Current timestamp: {}", timestamp);
+    let web3 = init_connection().unwrap();
+    check_on_config(&config, &user.address, &web3).await.unwrap();
 
     let abi = read_abi().unwrap();
     Ok(())
 }
 
-fn get_unix_time() -> u64 {
-    SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()
-}
-
-fn helper() {
-    Command::new("Snipe this mint!")
-        .about("A simple CLI tool to mint NFTs on Ethereum")
-        .version("0.1.0")
-        .author("Querty for The Diggers DAO")
-        .arg(
-            Arg::new("Mint contract address")
-                .short('c')
-                .long("contract_address")
-                .required(false),
-        )
-        .arg(
-            Arg::new("Contract ABI")
-                .short('a')
-                .long("abi")
-                .required(false),
-        )
-        .arg(
-            Arg::new("Price in WEI")
-                .short('p')
-                .long("price")
-                .required(false),
-        )
-        .arg(
-            Arg::new("Amount of NFTs to mint")
-                .short('n')
-                .long("amount")
-                .required(false),
-        )
-        .arg(
-            Arg::new("Timestamp of the mint in seconds")
-                .short('t')
-                .long("timestamp")
-                .required(false),
-        )
-        .get_matches();
-}
-
-async fn get_user_balance(web3: &web3::Web3<Http>, user: &str) -> web3::Result<()> {
-    let account: Address = user.parse().unwrap();
-
-    let wei_balance = web3.eth().balance(account, None).await?;
-    let eth_balance = Unit::Wei(&wei_balance.to_string()).to_eth_str().unwrap();
-    println!("Balance of {:?}: {} ETH", account, eth_balance);
-    Ok(())
-}
-
-fn read_abi() -> Result<Value>{
-    let mut file = File::open("abi/test.json").unwrap();
-
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    
-
-    let abi_json: Value = serde_json::from_str(&contents).unwrap();
-    let abi_string = abi_json["result"].as_str().unwrap();
-    let abi: Value = serde_json::from_str(abi_string).unwrap();
-    Ok(abi)
-}
+// mockERC721 contract deployed on goerli: 0x2F7F6a9cE73529354fc62bd42632da7A5084CC27
