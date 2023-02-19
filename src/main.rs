4e1b868ce8fb56fe0d2210e5a665e4ac0715ffed
Quertyy <qwertyense@protonmail.com>
2023-02-19 20:36:06 +0100

    refactor: change loop sleep from 1 sec to 10 ms

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ fn trigger_timestamp(timestamp: u64) {
     loop {
         let current_time = get_unix_time();
         if current_time < timestamp {
-            thread::sleep(Duration::from_secs(1));
+            thread::sleep(Duration::from_millis(10));
         } else {
             break;
         }
